c5888baf7218e5c1fffbbfe2e3577afe097106e2
Ife Onuorah <ifeonuorah@gmail.com>
2025-08-26 16:22:50 -0300

    structure and style hero section on homepage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,5 +1,15 @@
 import "./Home.scss";
 
 export default function Home() {
-	return <div>Home Page</div>;
+	return (
+		<main className="home">
+			<div className="hero">
+				<h1 className="hero__title">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+					minim veniam, quis nostrud exercitation ullamco laboris.
+				</h1>
+			</div>
+		</main>
+	);
 }
